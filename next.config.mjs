f61fa9b7dbfc2fbd24221c4dece7ab89505f1935
ChetanSo4 <chetanya4147@gmail.com>
2024-09-23 15:27:11 +0530

    some Changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@
 const nextConfig = {
     experimental:{
         ppr:"incremental" //The 'incremental' value allows you to adopt PPR for specific routes.
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
+
 
 export default nextConfig;
