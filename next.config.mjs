47c0d54f113616beb5bd31c65198817279d3d015
ChetanSo4 <chetanya4147@gmail.com>
2024-09-25 18:08:59 +0530

    auth file update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: '/dashboard',
     experimental:{
         ppr:"incremental" //The 'incremental' value allows you to adopt PPR for specific routes.
     }
