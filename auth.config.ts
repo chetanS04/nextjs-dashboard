8fc164c543c6fa551febb44c606a6de192c82532
ChetanSo4 <chetanya4147@gmail.com>
2024-09-25 16:44:28 +0530

    some Changes

--- auth.config.ts
+++ auth.config.ts
@@ -17,5 +17,6 @@ export const authConfig = {
       return true;
     },
   },
+  secret: process.env.AUTH_SECRET,
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
